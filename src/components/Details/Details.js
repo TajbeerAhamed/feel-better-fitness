91efae5a7244129dac6d224cc82f27e131ba7065
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-09-29 05:00:49 +0600

    list-item-added

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import './Details.css'
 
-const Details = () => {
+const Details = ({list}) => {
+    console.log(list)
+    let activityTime=0
+    for(const lists of list){
+ activityTime=activityTime+lists.activityTime
+    }
     return (
-        <div>
+        <div className='list'>
             <h1>Tajbeer Ahamed Rimon</h1>
+            <div className='list-details'>
+                <div className='list-info'>
+                <h3>69kg</h3>
+                <p>Weight </p>
+                </div>
+                <div className='list-info'>
+                <h3>5.4</h3>
+                <p>Height</p>
+                </div>
+                <div className='list-info'>
+                <h3>20</h3>
+                <p>Age </p>
+                </div>
+            </div>
+            <h2>Add a Break</h2>
+            <div >
+               <button className='break-btn'>10s</button>
+               <button className='break-btn'>20s</button>
+               <button className='break-btn'>30s</button>
+               <button className='break-btn'>40s</button>
+            </div>
+
+            <h2>Activity Details</h2>
+            <h4>Activity Time: {activityTime}</h4>
+            <h4>Break Time: {}</h4>
+            <button className='activity-btn'>
+                <p>Activity Completed</p>
+            </button>
         </div>
     );
 };
